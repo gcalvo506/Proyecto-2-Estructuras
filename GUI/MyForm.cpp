d2bde2a0d60b4bdae7639d63250a544971021116
Gerald Calvo <geraldcalvoah@gmail.com>
2019-05-06 23:07:02 -0600

    Se modifica el main()

--- GUI/MyForm.cpp
+++ GUI/MyForm.cpp
@@ -1,16 +1,15 @@
 #include "MyForm.h"
 
-using namespace GUI;    // Reemplazar Project1 por el nombre que se le haya dado al proyecto
+using namespace GUI;
 using namespace System;
 using namespace System::Windows::Forms;
 using namespace System::Globalization;
 
 [STAThread]
 
-void main(/*array<String^>^ arg*/) {
+void main() {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	//Project1 es el nombre del proyecto
-	MyForm form;	//Reemplazar MyForm por el nombre que tenga tu clase 
+	MyForm form;
 	Application::Run(%form);
 }
